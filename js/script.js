86f88d6507dceaa11633efc41447ca4cc6367ba1
LexaRunner <leha.polihim@gmail.com>
2021-03-28 13:21:16 +0500

    Урок 10 Функции

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let money = prompt("Ваш бюджет на месяц?", ''),
+let money = +prompt("Ваш бюджет на месяц?", ''),
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
@@ -10,8 +10,30 @@ let appData = {
     saving: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
-    a2 = prompt("Во сколько обойдётся?");
+for (let i = 0; i < 2; i++) {
+    
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдётся?", "");
+    
+    if( (typeof(a)) === 'string' && (typeof(a) != null) && (typeof(b) != null) && a != '' && b != '' && a.length < 50) {
+        console.log('done');
+        appData.expensive[a] = b;
+    } else {
 
-appData.expensive.a1 = a2;
-alert(appData.budget / 30);
+    }
+};
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка")
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Что то пошло не так!!!');
+};
+console.log(appData);
